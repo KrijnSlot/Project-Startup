b593dca9b5eec44d9fe17e5abb55d8175825f577
KrijnSlot <krijnslot112233@gmail.com>
2025-01-23 11:47:41 +0100

    shark script without animations

--- Project Startup/Assets/Scripts/SharkScript.cs
+++ Project Startup/Assets/Scripts/SharkScript.cs
@@ -4,13 +4,48 @@ using UnityEngine;
 
 public class SharkScript : MonoBehaviour
 {
+    public GameObject boat;
+    public float sharkSpeed = 5f;
+    public LayerMask isBoat;
+
+    private bool inRange;
+
+    Rigidbody rb;
+    Vector3 directionToBoat;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.useGravity = false; 
     }
 
     void Update()
     {
-        
+        MoveTowardBoat();
+        CheckBoatDistance();
+    }
+    void CheckBoatDistance()
+    {
+        inRange = Physics.CheckSphere(transform.position, 1f, isBoat);
+        if (inRange) { Debug.Log("in range"); }
+        else { Debug.Log("ah hell nah"); }
+    }
+
+    void MoveTowardBoat()
+    {
+        if (!inRange)
+        {
+            transform.LookAt(boat.transform);
+            directionToBoat = (boat.transform.position - transform.position).normalized;
+            rb.AddForce(directionToBoat * sharkSpeed, ForceMode.Force);
+        }
+        else
+        {
+            transform.LookAt(boat.transform);
+            Vector3 xLock = transform.eulerAngles;
+            xLock.x = 0f;
+            transform.rotation = Quaternion.Euler(xLock);
+            rb.AddForce(directionToBoat * (-sharkSpeed / 40), ForceMode.Force);
+        }
     }
 }
